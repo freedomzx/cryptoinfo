1a45bd7ba80ec09c3a1ff0fd0f15e3da23bcfbe6
freedomzx <lejongoco@gmail.com>
2021-10-19 03:28:46 -0400

    fix recommendation, layout, added more exchanges

--- app.py
+++ app.py
@@ -1,40 +1,54 @@
 from flask import Flask, render_template, request, url_for
 import requests
 import sys
+import json
 
 app = Flask(__name__)
 
-exchanges = ['Kraken', 'Binance']
+exchanges = ['Kraken', 'Binance', 'Bitstamp', 'Bitfinex', 'Coinbase-Pro', "FTX", "Bitbay"]
 currencies = ['Bitcoin', 'Ethereum']
 currencyToSymbol = {
     "Bitcoin": "btcusdt",
     "Ethereum": "ethusdt"
 }
 
+#landing point
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", exchanges=exchanges, currencies=currencies)
 
+#launches when button is submitted for crypto choice
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     currencyInfo = {}
+    validExchanges = []
+    #get response from dropdown menu
     if request.method == "POST":
 
         curr = request.form.get("currencySelect")
 
         for e in exchanges:
             req = requests.get("https://api.cryptowat.ch/markets/{}/{}/orderbook?limit=1".format(e.lower(), currencyToSymbol[curr]))
-            req = req.json()
-            currencyInfo[e] = req
+            req_json = req.json()
+
+            #check if exchange+symbol pair has no market
+            json_dict = json.loads(req.text)
+            if "error" in json_dict:
+                print("nonexistent market/symbol pair - {}".format(e), flush=True)
+                continue
+            validExchanges.append(e)
+            currencyInfo[e] = req_json
 
         #print(curr, flush=True)
 
+        #get recommended place tobuy/sell currency from all exchanges
         bestBuy = ""
         bestBuyPrice = 999999999
         bestSell = ""
         bestSellPrice = 0
 
+        print(currencyInfo.keys(), flush=True)
         for i in currencyInfo.keys():
             if currencyInfo[i]["result"]["asks"][0][0] < bestBuyPrice:
                 bestBuyPrice = currencyInfo[i]["result"]["asks"][0][0]
@@ -43,8 +57,8 @@ def form():
             if currencyInfo[i]["result"]["bids"][0][0] > bestSellPrice:
                 bestSellPrice = currencyInfo[i]["result"]["bids"][0][0]
                 bestSell = i
-
-    return render_template("index.html", exchanges=exchanges, currencies=currencies, curr=curr, currencyInfo=currencyInfo, bestBuy=bestBuy, bestSell=bestSell)
+    #re-render page with new information gotten
+    return render_template("index.html", exchanges=validExchanges, currencies=currencies, curr=curr, currencyInfo=currencyInfo, bestBuy=bestBuy, bestSell=bestSell)
 
 if __name__=='__main__':
     app.run(debug=True)
